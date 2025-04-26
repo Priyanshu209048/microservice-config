3ec0b5f802f9fe8a0687404b06c44cd95753029b
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2025-04-26 22:10:44 +0530

    Delete UserService directory

--- UserService/src/main/java/com/project/userservice/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.userservice.repo;
-
-import com.project.userservice.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, String> {
-}
