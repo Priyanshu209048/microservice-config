4c98dcda88ffef53b3c1301131a5852e79381d1a
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2025-04-26 22:10:10 +0530

    Delete HotelService directory

--- HotelService/src/main/java/com/project/hotelservice/repo/HotelRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.hotelservice.repo;
-
-import com.project.hotelservice.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepo extends JpaRepository<Hotel, String> {
-}
